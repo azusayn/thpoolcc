b18cf40a70c299f6a0cc9cf902195bba9a3234d7
root <azusaings@gmail.com>
2026-03-08 17:19:25 +0000

    refactor: combine 2 mutex into 1

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,22 @@
 #include "thpoolcc.h"
+#include <atomic>
+#include <iostream>
+
+void *f(void *args) {
+  auto count_ptr = (std::atomic<int32_t> *)(args);
+  for (int i = 0; i < 100000000; i++) {
+    count_ptr->fetch_add(1);
+  }
+  return nullptr;
+}
 
 int main(int argc, char *argv[]) {
+  ThreadPoolCC::ThreadPool thp(3);
+  std::atomic<int32_t> count(0);
+  for (int i = 0; i < 4; i++) {
+    thp.AddWork(f, (void *)&count);
+  }
+  thp.Wait();
+  std::cout << count << "\n";
   return 0;
 }
